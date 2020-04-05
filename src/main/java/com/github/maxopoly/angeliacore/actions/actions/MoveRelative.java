3f4b5c7afe874f827499c3ad1153c7ae55271f69
Maxopoly <maxopoly3@gmail.com>
2020-04-06 01:21:07 +0200

    Add relative movement action

---
+++ src/main/java/com/github/maxopoly/angeliacore/actions/actions/MoveRelative.java
@@ -0,0 +1,67 @@
+package com.github.maxopoly.angeliacore.actions.actions;
+
+import java.io.IOException;
+
+import com.github.maxopoly.angeliacore.actions.AbstractAction;
+import com.github.maxopoly.angeliacore.actions.ActionLock;
+import com.github.maxopoly.angeliacore.connection.ServerConnection;
+import com.github.maxopoly.angeliacore.connection.play.packets.out.PlayerPositionPacket;
+import com.github.maxopoly.angeliacore.model.ThePlayer;
+import com.github.maxopoly.angeliacore.model.location.DirectedLocation;
+import com.github.maxopoly.angeliacore.model.location.Location;
+import com.github.maxopoly.angeliacore.model.location.Vector;
+
+public class MoveRelative extends AbstractAction {
+	
+	private int remainingTicks;
+	private Vector direction;
+	private double speed;
+
+	public MoveRelative(ServerConnection connection, Vector direction, int ticks, double speed) {
+		super(connection);
+		this.remainingTicks = ticks;
+		this.direction = new Vector(direction.getX(), 0, direction.getZ());
+		this.direction.normalize();
+	}
+
+	@Override
+	public void execute() {
+		double ticksPerSecond = connection.getTicksPerSecond();
+		ThePlayer status = connection.getPlayerStatus();
+		status.updateLocation(getNextLocation(status.getLocation(), speed, ticksPerSecond));
+		sendLocationPacket();
+		if (isDone()) {
+			connection.getPlayerStatus().setOnGround(true);
+		}
+	}
+	
+	public DirectedLocation getNextLocation(DirectedLocation current, double movementSpeed, double ticksPerSecond) {
+		double movementPerTick = movementSpeed / ticksPerSecond;
+		double xDiff = direction.getX() * movementPerTick;
+		double zDiff = direction.getZ() * movementPerTick;
+		return new DirectedLocation(current.getX() + xDiff, current.getY(), current.getZ() + zDiff, current.getYaw(),
+				current.getPitch());
+	}
+
+	@Override
+	public ActionLock[] getActionLocks() {
+		return new ActionLock[] { ActionLock.MOVEMENT };
+	}
+
+	@Override
+	public boolean isDone() {
+		return remainingTicks <= 0;
+	}
+	
+	public void sendLocationPacket() {
+		Location playerLoc = connection.getPlayerStatus().getLocation();
+		try {
+			boolean onGround = connection.getPlayerStatus().isOnGround();
+			connection.sendPacket(
+					new PlayerPositionPacket(playerLoc.getX(), playerLoc.getY(), playerLoc.getZ(), onGround));
+		} catch (IOException e) {
+			connection.getLogger().error("Failed to update location", e);
+		}
+	}
+
+}
